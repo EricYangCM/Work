d399d98c98e7e3d387ef5b7e5df30a6beba5a0fe
EricYangCM <106502039+EricYangCM@users.noreply.github.com>
2025-02-21 14:35:24 +0900

    Create DS_PCR_Prototype_v1.cs

---
+++ DiasureBio/DS_PCR_Prototype_v1.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DS_PCR_Prototype_Test_v1
+{
+    internal class DS_PCR_Prototype_v1
+    {
+    }
+}
